ec0b2c6a03c4dfc7cc9610e9d3eb4a954fc41206
Joshua Hui <joshua.hui@Calabrio.com>
2019-09-20 19:17:10 -0700

    Created base.html and added Navbar.vue

---
+++ frontend/src/views/Example.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="example">
+    <h1>First Single File Componenet</h1>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "example",
+    data() {
+        return {
+            message: "Hello World Example!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+
+
+</style>
